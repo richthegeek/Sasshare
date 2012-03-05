0c3f9def841d43306a066c71a24c8d3a36545752
Richard Lyon <richthegeek@gmail.com>
2012-03-05 13:01:41 +0000

    .

---
+++ db.sql
@@ -0,0 +1,7 @@
+DROP DATABASE ;
+CREATE DATABASE ;
+USE ;
+Usage: mysqldump [OPTIONS] database [tables]
+OR     mysqldump [OPTIONS] --databases [OPTIONS] DB1 [DB2 DB3...]
+OR     mysqldump [OPTIONS] --all-databases [OPTIONS]
+For more options, use mysqldump --help
